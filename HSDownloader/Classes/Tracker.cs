a226aa7f99409969e910be4f24866d0dc1f2b2b2
Gary <g-gill24@Live.ca>
2016-06-03 00:53:10 -0400

    Update Tracker.cs

--- HSDownloader/Classes/Tracker.cs
+++ HSDownloader/Classes/Tracker.cs
@@ -88,7 +88,7 @@ namespace HorribleSubsTorrentDownloader.Classes
             {
                 Console.WriteLine("PhantomJS must be placed in: " + FileHandler.directoryPath);
                 Console.WriteLine("You can download PhantJS from: " + "http://phantomjs.org/download.html");
-                Console.ReadKey();
+                Console.ReadLine();
             }
             
         }
